345abf0cfa1e3300fd48cf2d938901359ebc8785
cerramaximiliano <cerramaximiliano@gmail.com>
2023-12-02 19:18:15 -0300

    Basic Node and Express 7/12

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ let express = require('express');
 let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
+app.use(
+    function(req, res, next) {
+        console.log(`${req.method} ${req.path} - ${req.ip}`)
+        next();
+      }
+)
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
