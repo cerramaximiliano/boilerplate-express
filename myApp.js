336740eaa0661935dbe55e033c403936a5df9f7a
cerramaximiliano <cerramaximiliano@gmail.com>
2023-12-02 19:31:34 -0300

    Basic Node and Express 9/12

--- myApp.js
+++ myApp.js
@@ -34,6 +34,12 @@ app.get("/now", function(req,res,next){
     })
 });
 
+app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({
+      echo: word
+    });
+});
 
 
 
